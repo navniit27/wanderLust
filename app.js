e9db438945bc71e9a38cb638477a5fd242c3b953
Navnit <snavnit272705@gmail.com>
2026-08-23 15:11:46 +0530

    spelling mistake in expresserror

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const LocalStrategy = require("passport-local");
 const helmet = require("helmet");
 const compression = require("compression");
 
-const ExpressError = require("./utils/ExpressError");
+const ExpressError = require("./utils/expressError");
 
 const User = require("./models/user");
 
