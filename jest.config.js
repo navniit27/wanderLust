79c9cc6d8ae11b2d90a26738696a4c4f44a958c2
Navnit kumar <snavnit2705@gmail.com>
2026-08-25 12:35:34 +0530

    chore: remove unused Jest configuration

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-process.env.NODE_ENV = "test";
-process.env.SECRET = process.env.SECRET || "test-secret-value-for-jest";
-
-module.exports = {
-    testEnvironment: "node",
-    testTimeout: 60000,
-    verbose: true,
-};
